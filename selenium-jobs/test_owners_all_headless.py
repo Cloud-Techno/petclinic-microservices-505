44d862daf04966f9c23e856fc57a4688d1e2f28a
Cloud-Techno <ferdiates157@gmail.com>
2023-11-17 09:49:14 +0000

    added qa automation pipeline for dev

--- selenium-jobs/test_owners_all_headless.py
+++ selenium-jobs/test_owners_all_headless.py
@@ -4,11 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import os
+
 # Set chrome options for working with headless mode (no screen)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 chrome_options.add_argument("no-sandbox")
 chrome_options.add_argument("disable-dev-shm-usage")
+
 # Update webdriver instance of chrome-driver with adding chrome options
 driver = webdriver.Chrome(options=chrome_options)
 # driver = webdriver.Chrome("/Users/home/Desktop/chromedriver")
@@ -25,8 +27,11 @@ sleep(2)
 all_link = driver.find_element("link text","ALL")
 all_link.click()
 sleep(2)
+
 # Verify that table loaded
 sleep(1)
 verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
+
 print("Table loaded")
+
 driver.quit()
